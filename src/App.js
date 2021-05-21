3f6d5e23ce3a25e8aafaabcdb9dbcd960d2581ba
Shobha Bhat <shobha.bhat36@gmail.com>
2021-05-22 00:33:12 +0530

    Initial app structure creation

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import WeatherWidget from './component/WeatherWidget';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import App from './App';
+import WeatherWidget from './component/WeatherWidget';
+
+describe('App', () => {
+  it('renders App', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).not.toBeNull();
+  });
+
+  it('renders weather component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists(WeatherWidget)).toBeTruthy();
+  });
+
+});
